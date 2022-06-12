45cea177c2aff33757a221b3669cb30d53b9a89f
darkmift <darkmift@gmail.com>
2022-06-12 21:23:48 +0300

    migrated key to env file

--- src/services/accuWeather.js
+++ src/services/accuWeather.js
@@ -4,41 +4,42 @@ import store from '../store'
 
 export default class accuWeather {
 
-    async suggestLocation(query){
+    #apiKey = process.env.VUE_APP_API_KEY;
+    async suggestLocation(query) {
         let url = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete"
         let results = await axios.get(url, {
             params: {
-              apikey: 'mCzmCuzMmsGz9cL6x4rVqUheFJ0l9WAI',
-              q:query
+                apikey: this.#apiKey,
+                q: query
             }
         })
         return results.data
     }
 
-    async getFiveDayForecast(locationKey){
-        let url =`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}`
+    async getFiveDayForecast(locationKey) {
+        let url = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}`
         let results = await axios.get(url, {
             params: {
-              apikey: 'mCzmCuzMmsGz9cL6x4rVqUheFJ0l9WAI',
-              details: true,
-              metric: store.state.isMetric
+                apikey: this.#apiKey,
+                details: true,
+                metric: store.state.isMetric
 
             }
         })
         return results.data['DailyForecasts']
     }
 
-    async getFavouriteData(locationKey){
-        let url =`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}`
+    async getFavouriteData(locationKey) {
+        let url = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}`
         let results = await axios.get(url, {
             params: {
-              apikey: 'mCzmCuzMmsGz9cL6x4rVqUheFJ0l9WAI',
-              details: true,
-              metric: store.state.isMetric
+                apikey: this.#apiKey,
+                details: true,
+                metric: store.state.isMetric
 
             }
         })
         return results.data['DailyForecasts']
     }
-  
+
 }
