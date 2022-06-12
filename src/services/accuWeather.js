d02b45ac69c878fec62292757ac28163f728db7f
Sagi <sagix7004@gmail.com>
2022-06-12 15:47:31 +0300

    first commit

---
+++ src/services/accuWeather.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import store from '../store'
+
+
+export default class accuWeather {
+
+    async suggestLocation(query){
+        let url = "http://dataservice.accuweather.com/locations/v1/cities/autocomplete"
+        let results = await axios.get(url, {
+            params: {
+              apikey: 'mCzmCuzMmsGz9cL6x4rVqUheFJ0l9WAI',
+              q:query
+            }
+        })
+        return results.data
+    }
+
+    async getFiveDayForecast(locationKey){
+        let url =`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}`
+        let results = await axios.get(url, {
+            params: {
+              apikey: 'mCzmCuzMmsGz9cL6x4rVqUheFJ0l9WAI',
+              details: true,
+              metric: store.state.isMetric
+
+            }
+        })
+        return results.data['DailyForecasts']
+    }
+
+    async getFavouriteData(locationKey){
+        let url =`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}`
+        let results = await axios.get(url, {
+            params: {
+              apikey: 'mCzmCuzMmsGz9cL6x4rVqUheFJ0l9WAI',
+              details: true,
+              metric: store.state.isMetric
+
+            }
+        })
+        return results.data['DailyForecasts']
+    }
+  
+}
