492da473b29e7aaf29c9678d80a2761e0c95eaef
Sagi <sagix7004@gmail.com>
2022-06-12 22:27:01 +0300

    added js pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/sagi-cohen-09-06-2022/" : "/",
 })
